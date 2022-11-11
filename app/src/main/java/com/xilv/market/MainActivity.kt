7c67fd981b1cf22a4493bb86403559974b0c179e
nelson-XILV <khojugken@gmail.com>
2022-11-11 23:51:04 +0300

    initial commit

---
+++ app/src/main/java/com/xilv/market/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.xilv.market
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.Window
+import android.widget.Toolbar
+import com.xilv.market.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+}
+
